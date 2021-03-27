d9df9332f5790d60db662421d93d1918cf9a2f63
P. Cecchetto <p.cecchetto@newmotion.com>
2021-03-27 21:47:39 +0100

    minimal setup

---
+++ build.sbt
@@ -0,0 +1,30 @@
+import Dependencies._
+enablePlugins(ScalaJSPlugin)
+
+lazy val commonSettings = inThisBuild(
+    Seq(
+      scalaVersion := "2.13.1",
+      addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.0"),
+      addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
+    )
+)
+
+lazy val frontend =
+  (crossProject(JSPlatform).crossType(CrossType.Pure) in file(
+    "frontend"
+  )).disablePlugins(RevolverPlugin)
+    .settings(commonSettings)
+    .jsSettings(
+      scalaJSLinkerConfig ~= { _.withModuleKind(ModuleKind.CommonJSModule) },
+      libraryDependencies ++= Dependencies.Frontend.all.value,
+    )
+
+lazy val root = project
+  .in(file("."))
+  .settings(commonSettings)
+  .settings(
+    name := "laminar-tetris",
+    organization := "io.tuliplogic",
+    scalaVersion := "2.13.1",
+    libraryDependencies ++= Seq()
+  )
---
+++ project/Dependencies.scala
@@ -0,0 +1,39 @@
+import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
+import sbt._
+
+object Dependencies {
+  object Versions {
+    val http4s = "0.21.0-M6"
+
+    val zio       = "1.0.3"
+    val zioCats   = "2.2.0.1"
+
+    val fs2        = "2.4.4"
+    val cats       = "2.2.0"
+    val catsEffect = "2.2.0"
+    val circe      = "0.12.1"
+
+    val laminar    = "0.9.0"
+  }
+
+  object Frontend {
+    val all = Def.setting(
+      Seq(
+        "com.raquo" %%% "laminar" % Versions.laminar
+      )
+    )
+  }
+
+  val scalaTest  = "org.scalatest" %% "scalatest"   % "3.0.5"
+
+  val cats       = "org.typelevel" %% "cats-core"   % Versions.cats
+  val catsEffect = "org.typelevel" %% "cats-effect" % Versions.catsEffect
+
+  val zio         = "dev.zio"       %% "zio"              % Versions.zio
+  val zioStreams  = "dev.zio"       %% "zio-streams"      % Versions.zio
+  val zioCats     = "dev.zio"       %% "zio-interop-cats" % Versions.zioCats
+
+  val cask        =  "com.lihaoyi" %% "cask" % "0.6.5"
+
+  val fs2         = "co.fs2" %% "fs2-io" % Versions.fs2
+}
